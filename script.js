df086055bc8e2ed950c83f873e071788bf2d7cb8
Nithyasree8 <113105796+Nithyasree8@users.noreply.github.com>
2024-10-13 17:48:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //complete this code
-class Animal {}
+// Animal class definition
+class Animal {
+    constructor(species) {
+        this._species = species; // Private property
+    }
 
-class Dog extends Animal {}
+    get species() {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    makeSound() {
+        console.log(`The ${this.species} makes a sound`);
+    }
+}
+
+// Cat class definition
+class Cat extends Animal {
+    constructor(species) {
+        super(species);
+    }
+
+    purr() {
+        console.log('purr');
+    }
+}
+
+// Dog class definition
+class Dog extends Animal {
+    constructor(species) {
+        super(species);
+    }
+
+    bark() {
+        console.log('woof');
+    }
+}
+
 
 // Do not change the code below this line
 window.Animal = Animal;
